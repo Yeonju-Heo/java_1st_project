604288110e66e1c6cbc979088281c2cffb6cc297
희수 <@DESKTOP-80SB8PH>
2021-04-13 19:00:26 +0900

    MbtiMgmSystem 기본 틀

--- 1st_project/src/mbti_system/MbtiMgmSystem.java
+++ 1st_project/src/mbti_system/MbtiMgmSystem.java
@@ -1,5 +1,29 @@
 package mbti_system;
 
 public class MbtiMgmSystem {
-
+	//Field
+	
+	//로그인 결과
+	public static boolean LOGIN_RESULT = false;
+	
+	//Constructor
+	public MbtiMgmSystem() {
+		
+	}
+	
+	//Method
+	/** 로그인 **/
+	
+	/** 회원가입 **/
+	
+	/** 캐릭터생성 **/
+	
+	/** 채팅 **/
+	
+	/** 게시판 **/
+	
+	/** 마이페이지 **/
+	
+	/** 종료 **/
+	
 }
