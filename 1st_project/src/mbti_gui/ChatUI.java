12a3d4e18199345f30ae673a20fe5f86723097d0
Star3 <Star3@DESKTOP-9O53AGQ>
2021-04-16 17:25:47 +0900

    채팅 uservo, mbtivo연결

--- 1st_project/src/mbti_gui/ChatUI.java
+++ 1st_project/src/mbti_gui/ChatUI.java
@@ -1,10 +1,10 @@
 package mbti_gui;
+
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.FlowLayout;
 import java.awt.Frame;
 import java.awt.GridLayout;
-import java.awt.Panel;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseAdapter;
@@ -24,22 +24,22 @@ import javax.swing.JTextArea;
 import javax.swing.JTextField;
 import javax.swing.ScrollPaneConstants;
 
-import com.sun.tools.javac.Main;
-
+import mbti_vo.MbtiVO;
 import mbti_vo.MessageVO;
+import mbti_vo.UserVO;
 
 public class ChatUI implements ActionListener{
 	//Field
-	JFrame chat_frame;//프레임
+	JFrame chat_frame;//?��?��?��
 	
 	JPanel left_panel;
 	JPanel right_panel;
 	JPanel center_panel;
 	
 	JTextArea chat_content;
-	JTextField chat_tf;//text 보내는 곳
+	JTextField chat_tf;//text 보내?�� �?
 	JButton btn_send;
-	JList user_list;//유저 리스트
+	JList user_list;//?��?? 리스?��
 	
 	JPanel list_panel;
 	JPanel botton_panel;
@@ -63,30 +63,30 @@ public class ChatUI implements ActionListener{
 	}
 	
 	public void init(){	
-		chat_frame = new JFrame("MBTI WORLD");//챗 프레임
-		left_panel = new JPanel(new GridLayout());//채팅 올라오는 곳
-		right_panel = new JPanel(new GridLayout());//유저리스트 보여주는 곳 
-		center_panel = new JPanel();//전체 패널
-		botton_panel = new JPanel(new FlowLayout());//아래 패널
+		chat_frame = new JFrame("MBTI WORLD");//프레임 생성
+		left_panel = new JPanel(new GridLayout());//채팅 왼편 패널
+		right_panel = new JPanel(new GridLayout());//리스트 보여주는 패널 
+		center_panel = new JPanel();//내용패널
+		botton_panel = new JPanel(new FlowLayout());//버튼
 		ImageIcon chattitle = new ImageIcon("images/chattitle.png");
 		JLabel title_Label = new JLabel(chattitle);
 		
-		chat_content = new JTextArea(30,30);//text채팅 전체 창
+		chat_content = new JTextArea(30,30);//text채팅 내용 창
 		chat_tf = new JTextField(52);//텍스트 입력창
-		btn_send = new JButton("보내기");//보내기
-		user_list = new JList();//유저 리스트를 넣을 곳
+		btn_send = new JButton("보내기");//보내기 버튼
+		user_list = new JList();//참여 유저들을 보여줄 채팅 리스트 
 		
-		//채팅 컨텐츠 화면
+		//채팅 내용 패널
 		JScrollPane jp = new JScrollPane(chat_content);
 		jp.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
 		chat_content.setFont(Commons.getFont());
 		left_panel.add(jp);
 		
-		//채팅 오른쪽 화면
+		//채팅 오른쪽 패널
 		user_panel = new JPanel(new GridLayout());
-		la_user = new JLabel("채팅 접속자");
+		la_user = new JLabel("채팅 ?��?��?��");
 		la_user.setHorizontalAlignment(JLabel.CENTER);
-		la_count = new JLabel("0명 접속중");
+		la_count = new JLabel("0�? ?��?���?");
 		la_count.setHorizontalAlignment(JLabel.CENTER);
 		la_user.setFont(Commons.getFont());
 		user_list.setFont(Commons.getFont());
@@ -100,7 +100,7 @@ public class ChatUI implements ActionListener{
 		right_panel.add("Center",user_panel);
 		right_panel.add("South", la_count);
 		
-		//아래 채팅 보내는 곳
+		//채팅 보내는 장소
 		chat_tf.setFont(Commons.getFont());
 		btn_send.setFont(Commons.getFont());
 	//			JPanel setbotton_panel = new JPanel(new FlowLayout());
@@ -120,21 +120,21 @@ public class ChatUI implements ActionListener{
 		chat_frame.add(BorderLayout.NORTH, title_Label);
 		
 		chat_frame.setSize(700,600);
-		chat_frame.setResizable(false);//사이즈 고정
+		chat_frame.setResizable(false);//채팅 사이즈 고정
 		chat_frame.setVisible(true);
 		
 		chat_tf.addActionListener(this);
 		btn_send.addActionListener(this);
 		
-		//유저 리스트를 클릭해서 프로필 보기
+		//유저리스트를 클릭해서 유저 프로필 보기
 		user_list.addMouseListener(new MouseAdapter() {
 			public void mouseClicked(MouseEvent evt) {
 				JList list = (JList)evt.getSource();
 				if(evt.getClickCount() == 2) {
-					//더블 클릭시 이벤트 실행
+					//클릭 이벤트
 					int index = list.locationToIndex(evt.getPoint());
 					Frame image = new Frame("유저 캐릭터");
-					//DB에서 전송받은 이미지를 넣는 Label, panel
+					//DB에서 사진 불러오기
 					ImageIcon img_char = new ImageIcon("images/character.png");
 					JLabel image_Label = new JLabel(img_char);
 					JPanel image_Panel = new JPanel();
@@ -162,9 +162,10 @@ public class ChatUI implements ActionListener{
 			public void windowClosing(WindowEvent e) {
 				try {
 					//Exit
-					System.out.println("끝내기");
+					System.out.println("?��?���?");
 					MessageVO vo = new MessageVO();
-					vo.setName("user_name"); //나갈때 이름
+					UserVO uvo = new UserVO();
+					vo.setName(uvo.getU_id()); //퇴장 아이디
 					vo.setStatus(MessageVO.EXIT);			
 					client.oos.writeObject(vo);
 					
@@ -189,12 +190,14 @@ public class ChatUI implements ActionListener{
 				try {
 					//talk
 					MessageVO vo = new MessageVO();
-					vo.setName("user_name"); //채팅중 이름 
-					vo.setMbti("mbti");
+					UserVO uvo = new UserVO();
+					MbtiVO mvo = new MbtiVO();
+					vo.setName(uvo.getU_id()); //말할때 아이디
+					vo.setMbti(mvo.getMbti_type());
 					vo.setMsg(chat_tf.getText());
 					vo.setStatus(MessageVO.TALK);
 					
-					//보내기
+					//보내�?
 					client.oos.writeObject(vo);
 					chat_tf.setText("");
 					
@@ -202,7 +205,7 @@ public class ChatUI implements ActionListener{
 					e2.printStackTrace();
 				}
 			}else {
-				JOptionPane.showMessageDialog(null, Commons.getMsg("데이터를 입력해주세요."));
+				JOptionPane.showMessageDialog(null, Commons.getMsg("?��?��?���? ?��?��?��주세?��."));
 				chat_tf.requestFocus();
 			}
 	}
